a3ed83b3dffae4d7dc20344915d55550a7554997
VALLIPI CHOWDAPPA <vallipichowdappa@VALLIPIs-MacBook-Air.local>
2025-05-23 00:37:12 +0530

    Wire up pages and routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import PatientRegistration from './pages/PatientRegistration';
 import PatientQuery from './pages/PatientQuery';
